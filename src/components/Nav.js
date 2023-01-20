31e30f1bbd1117ee9dac3b4beddee0a3ec56aa73
Tayebe kavousi <tayebekavousi68@gmail.com>
2023-01-20 23:44:14 +0800

    adding logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,19 @@
 import {Container, Navbar} from "react-bootstrap";
-
+import logo from "./logo.png"
 const Nav = () => {
     return (
         <Navbar bg="light"  variant="light" sticky="top">
             <Container>
-                <Navbar.Brand href="#home">LapTop Shop!!!</Navbar.Brand>
+                <Navbar.Brand href="#home">
+                    <img
+                        src={logo}
+                        width="150"
+                        height="150"
+                        className="d-inline-block align-top"
+                        alt="logo"
+                    />
+                </Navbar.Brand>
+                <Navbar.Brand href="#home">Home</Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end">
                     <Navbar.Text>
